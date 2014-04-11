a3b434166defdb19e0da10b4f26194cf198ccc0b
Balaji Narain <narainbalaji@gmail.com>
2014-04-11 13:34:34 +0530

    BalajiN | Creating a hello world message

---
+++ src/main/java/org/freeshr/web/controller/DefaultController.java
@@ -0,0 +1,22 @@
+package org.freeshr.web.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/patient")
+public class DefaultController {
+
+
+    @RequestMapping("/message")
+    public String simpleMessage() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DefaultController.class, args);
+    }
+}
