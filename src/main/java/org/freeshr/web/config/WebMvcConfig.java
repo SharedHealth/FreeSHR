7b51b2c0ce00a177c481f839d1592332b50d0b0e
Balaji Narain <narainbalaji@gmail.com>
2014-04-11 13:59:42 +0530

    BalajiN | Adding checkstyle and correcting bugs

--- src/main/java/org/freeshr/web/config/WebMvcConfig.java
+++ src/main/java/org/freeshr/web/config/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package org.freeshr.web.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
--- src/main/java/org/freeshr/web/controller/DefaultController.java
+++ src/main/java/org/freeshr/web/controller/DefaultController.java
@@ -1,7 +1,5 @@
 package org.freeshr.web.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
