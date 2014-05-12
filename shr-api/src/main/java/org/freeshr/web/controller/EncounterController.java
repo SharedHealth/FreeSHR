3d63d2ca90d29ead2565e0de3ced39a8f4aadeae
Balaji Narain <narainbalaji@gmail.com>
2014-05-12 21:21:32 +0530

    BalajiN | #49 | Packaging the application as a runnable war

--- shr-api/src/main/java/org/freeshr/web/controller/EncounterController.java
+++ shr-api/src/main/java/org/freeshr/web/controller/EncounterController.java
@@ -1,5 +1,6 @@
 package org.freeshr.web.controller;
 
+import org.freeshr.encounter.service.EncounterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/encounter")
 public class EncounterController {
 
+    private EncounterService encounterService;
 
     @Autowired
-    public EncounterController() {
+    public EncounterController(EncounterService encounterService) {
+        this.encounterService = encounterService;
     }
 
     @RequestMapping(method = RequestMethod.POST)
