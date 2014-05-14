2fad4c322557aa9ce08746eb8913b0f08c304378
Balaji Narain <narainbalaji@gmail.com>
2014-05-14 10:25:33 +0530

    BalajiN | #49 | Making ITs runnable from idea

--- shr/src/test/java/org/freeshr/shr/patient/wrapper/MasterClientIndexWrapperTest.java
+++ shr/src/test/java/org/freeshr/shr/patient/wrapper/MasterClientIndexWrapperTest.java
@@ -1,5 +1,6 @@
 package org.freeshr.shr.patient.wrapper;
 
+import org.freeshr.shr.config.EnvironmentMock;
 import org.freeshr.shr.config.SHRConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +12,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = SHRConfig.class)
+@ContextConfiguration(initializers = EnvironmentMock.class, classes = SHRConfig.class)
 public class MasterClientIndexWrapperTest {
 
     private final String validHealthId = "10";
