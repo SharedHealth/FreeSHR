693337314c023cd0bc87fb745465212a15ddf50f
Balaji Narain <narainbalaji@gmail.com>
2014-05-15 10:27:57 +0530

    BalajiN | #49 | Registering and unregistering bdshr service

--- shr-api/scripts/utils/postInstall.sh
+++ shr-api/scripts/utils/postInstall.sh
@@ -3,3 +3,6 @@
 ln -s /opt/bdshr/bin/bdshr /etc/init.d/bdshr
 ln -s /opt/bdshr/etc/bdshr /etc/default/bdshr
 ln -s /opt/bdshr/var /var/run/bdshr
+
+# Add bdshr service to chkconfig
+chkconfig --add bdshr
--- shr-api/scripts/utils/postUninstall.sh
+++ shr-api/scripts/utils/postUninstall.sh
@@ -3,3 +3,6 @@
 rm -f /etc/init.d/bdshr
 rm -f /etc/default/bdshr
 rm -f /var/run/bdshr
+
+#Remove bdshr from chkconfig
+chkconfig --del bdshr
