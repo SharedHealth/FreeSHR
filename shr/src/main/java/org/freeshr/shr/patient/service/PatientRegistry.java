214a73874357aaa99e970186a405ddc9956524c3
Balaji Narain <narainbalaji@gmail.com>
2014-05-19 18:41:47 +0530

    BalajiN | #49 | Replacing complicated wrapper logic with lazy evaluation

--- shr/src/main/java/org/freeshr/shr/patient/service/PatientRegistry.java
+++ shr/src/main/java/org/freeshr/shr/patient/service/PatientRegistry.java
@@ -1,15 +1,12 @@
 package org.freeshr.shr.patient.service;
 
-import com.google.common.util.concurrent.SettableFuture;
-import org.freeshr.shr.concurrent.NotNull;
-import org.freeshr.shr.concurrent.SimpleListenableFuture;
 import org.freeshr.shr.patient.model.Patient;
 import org.freeshr.shr.patient.repository.AllPatients;
 import org.freeshr.shr.patient.wrapper.MasterClientIndexWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.util.concurrent.ListenableFutureAdapter;
 
 import java.util.concurrent.ExecutionException;
 
@@ -26,36 +23,23 @@ public class PatientRegistry {
     }
 
     public ListenableFuture<Boolean> isValid(final String healthId) {
-        final SettableFuture<Boolean> future = SettableFuture.create();
-        new NotNull<Patient>(allPatients.find(healthId)).addCallback(new ListenableFutureCallback<Boolean>() {
+        return new ListenableFutureAdapter<Boolean, Patient>(allPatients.find(healthId)) {
             @Override
-            public void onSuccess(Boolean result) {
-                if (result) {
-                    future.set(Boolean.TRUE);
+            protected Boolean adapt(Patient result) throws ExecutionException {
+                if (null == result) {
+                    try {
+                        return new ListenableFutureAdapter<Boolean, Boolean>(masterClientIndexWrapper.isValid(healthId)) {
+                            @Override
+                            protected Boolean adapt(Boolean result) throws ExecutionException {
+                                return result;
+                            }
+                        }.get();
+                    } catch (Exception e) {
+                        throw new ExecutionException(e);
+                    }
                 } else {
-                    masterClientIndexWrapper.isValid(healthId).addCallback(new ListenableFutureCallback<Boolean>() {
-                        @Override
-                        public void onSuccess(Boolean result) {
-                            future.set(result);
-                        }
-
-                        @Override
-                        public void onFailure(Throwable t) {
-                            future.setException(t);
-                        }
-                    });
+                    return Boolean.TRUE;
                 }
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-                future.setException(t);
-            }
-        });
-        return new SimpleListenableFuture<Boolean, Boolean>(future) {
-            @Override
-            protected Boolean adapt(Boolean result) throws ExecutionException {
-                return result;
             }
         };
     }
