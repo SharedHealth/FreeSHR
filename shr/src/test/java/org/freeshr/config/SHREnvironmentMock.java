74e2b8f340a8fc8647959a0cc6f8ec73ac59fcdc
Johnvp <johnvp@john.corporate.thoughtworks.com>
2014-07-17 11:47:01 +0530

    John, Sumit | running tests on embedded cassandra server

--- shr/src/test/java/org/freeshr/config/SHREnvironmentMock.java
+++ shr/src/test/java/org/freeshr/config/SHREnvironmentMock.java
@@ -1,5 +1,7 @@
 package org.freeshr.config;
 
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.freeshr.utils.cassandra.Migrations;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
@@ -7,9 +9,9 @@ import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.StandardEnvironment;
 import org.springframework.mock.env.MockPropertySource;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 @Configuration
@@ -19,16 +21,24 @@ public class SHREnvironmentMock implements ApplicationContextInitializer<Configu
     public void initialize(ConfigurableApplicationContext applicationContext) {
         MutablePropertySources propertySources = applicationContext.getEnvironment().getPropertySources();
         MockPropertySource mockEnvVars = new MockPropertySource();
+        Map<String, String> env = new HashMap<String, String>();
         try {
             InputStream inputStream = this.getClass().getResourceAsStream("/test-shr.properties");
             Properties properties = new Properties();
             properties.load(inputStream);
             for (Object property : properties.keySet()) {
                 mockEnvVars.setProperty(property.toString(), properties.getProperty(property.toString()));
+                env.put(property.toString(), properties.getProperty(property.toString()));
             }
             propertySources.replace(StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, mockEnvVars);
         } catch (Exception ignored) {
             // Do nothing
+        }
+        try {
+            EmbeddedCassandraServerHelper.startEmbeddedCassandra();
+            new Migrations(env).migrate();
+        } catch (Exception e) {
+            throw new RuntimeException("Error starting embedded server..", e);
         }
     }
 }
