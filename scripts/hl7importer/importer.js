81dc05aa10492d0a04e056d18377728f018164f3
Johnvp <johnvp@john.corporate.thoughtworks.com>
2014-08-13 14:21:28 +0530

    John, Utsab | fixed category url issue

--- scripts/hl7importer/importer.js
+++ scripts/hl7importer/importer.js
@@ -3,6 +3,7 @@ var _ = require('underscore')
 var cheerio = require('cheerio')
 
 var baseUrl = 'http://hl7.org/implement/standards/fhir/';
+var url_pattern = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[{};:'".,<>?«»“”‘’]|\]|\?))/ig;
 
 function append(xs, x) {
     var list = xs || []
@@ -44,8 +45,8 @@ function extractCodes(category, handler) {
 
     getBody(category, function (body) {
         var $ = cheerio.load(body);
-        var codeElements = $('div[class=col-9] > div > table > tr > td > a');
-        handler(category, _.chain(codeElements).filter(isValidCode).map(toCodeValue).value().join(","));
+        var codeElements = $('div[class=col-9] div > table > tr > td > a');
+        handler($('div[class=col-9] > h1').text().match(url_pattern)[0], _.chain(codeElements).filter(isValidCode).map(toCodeValue).value().join(","));
     });
 }
 
@@ -59,4 +60,5 @@ extractCategories(baseUrl, 'terminologies-valuesets.html', function (categories)
         extractCodes(category, handler);
     })
 });
+
 
