65cfff0fbe4b5b0085792b7252b3a930d87a3feb
rnjn <runjan@gmail.com>
2015-03-20 18:26:01 +0530

    Fixing logging blanks and filtering out access logs from app log

---
+++ shr/src/main/java/org/freeshr/utils/log4j/NormalLogFilter.java
@@ -0,0 +1,12 @@
+package org.freeshr.utils.log4j;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class NormalLogFilter extends Filter {
+    @Override
+    public int decide(LoggingEvent event) {
+        return StringUtils.startsWith(event.getMessage().toString(), "ACCESS:") ? -1 : 1;
+    }
+}
